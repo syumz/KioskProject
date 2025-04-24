70f002e5ae67a47d14cdc41a746e997c9b2864f2
seoyeon <dlatjdus0303@naver.com>
2025-04-24 13:56:58 +0900

    feat: implement menu loop using do-while

--- src/Lv1/Lv1Main.java
+++ src/Lv1/Lv1Main.java
@@ -6,10 +6,9 @@ public class Lv1Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while(true) {
-            System.out.print("햄버거 메뉴를 보시려면 'menu'를 입력해주세요. : ");
+            System.out.print("햄버거 메뉴를 보시려면 'hamburger'를 입력해주세요. : ");
             String shakeshackMenu = scanner.nextLine();
-            if (shakeshackMenu.equals("menu")) {
+            if (shakeshackMenu.equals("hamburger")) {
                 System.out.println("[ SHAKESHACK MENU ]\n" +
                         "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
                         "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
@@ -17,12 +16,21 @@ public class Lv1Main {
                         "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
                         "0. 종료      | 종료");
             }
-            System.out.println("0을 입력하시면 종료됩니다.");
-            int exit = scanner.nextInt();
-            if (exit == 0) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            }
-        }
+            int num;
+            do{
+                System.out.print("원하는 메뉴를 선택해주세요. (0을 입력하시면 종료됩니다.): ");
+                num = scanner.nextInt();
+                if(num==1){
+                    System.out.println("ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                } else if(num==2){
+                    System.out.println("SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                } else if(num==3){
+                    System.out.println("Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                } else if(num==4){
+                    System.out.println("Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                }
+            }while(num!=0);
+        System.out.println("프로그램을 종료합니다.");
+
     }
 }

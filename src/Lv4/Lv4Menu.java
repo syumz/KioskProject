b5f92d4336574aa573c5641beb8313146aaf4df8
seoyeon <dlatjdus0303@naver.com>
2025-04-25 13:21:36 +0900

    feat: add menu list to Lv4menu and method to print it

--- src/Lv4/Lv4Menu.java
+++ src/Lv4/Lv4Menu.java
@@ -1,4 +1,46 @@
 package Lv4;
 
-public class Lv4Menu { // MenuItem 클래스를 관리하는 클래스
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lv4Menu {// MenuItem 클래스를 관리하는 클래스
+    // 1. 속성
+    List<Lv4MenuItem> menuItems = new ArrayList<>();
+    List<Lv4Menu> lv4Menus = new ArrayList<>();
+    private String name;
+
+    // 2. 생성자
+    public Lv4Menu(){
+    }
+
+    public Lv4Menu(String name){
+        this.name = name;
+    }
+
+    // 3. 기능(메서드)
+
+    public void burgersMenu(){
+        System.out.println("[ SHAKESHACK MENU ]");
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.print(i + 1 + ". ");
+            System.out.println(menuItems.get(i));
+        }
+        System.out.println("0. 뒤로가기");
+    }
+
+    public void mainMenu(){
+        System.out.println("[ MAIN MENU ]");
+        for (int i = 0; i < lv4Menus.size(); i++) {
+            System.out.print(i + 1 + ". ");
+            System.out.println(lv4Menus.get(i));
+        }
+        System.out.println("0. 종료");
+    }
+
+    public String toString(){
+        return name;
+    }
+
+
+
 }

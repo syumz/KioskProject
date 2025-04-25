58577f83e1c459a7b59cdba43e0a7a7cab8b7ce6
seoyeon <dlatjdus0303@naver.com>
2025-04-25 13:31:06 +0900

    feat: implement Lv4Main to populate menu and start kiosk flow

--- src/Lv4/Lv4Main.java
+++ src/Lv4/Lv4Main.java
@@ -2,6 +2,19 @@ package Lv4;
 
 public class Lv4Main {
     public static void main(String[] args) {
+        Lv4Menu lv4Menu = new Lv4Menu();
+
+        lv4Menu.menuItems.add(new Lv4MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        lv4Menu.menuItems.add(new Lv4MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        lv4Menu.menuItems.add(new Lv4MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        lv4Menu.menuItems.add(new Lv4MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        lv4Menu.lv4Menus.add(new Lv4Menu("Burgers"));
+        lv4Menu.lv4Menus.add(new Lv4Menu("Drinks"));
+        lv4Menu.lv4Menus.add(new Lv4Menu("Desserts"));
+
+        Lv4Kiosk lv4Kiosk = new Lv4Kiosk(lv4Menu);
+        lv4Kiosk.start();
 
     }
 }

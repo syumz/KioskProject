cff9f152ba26b0620cc353be5433fe88666cd908
seoyeon <dlatjdus0303@naver.com>
2025-04-28 19:31:03 +0900

    feat: add exception handling and comments to CLv1Cart

--- src/ChallengeLv1/CLv1Cart.java
+++ src/ChallengeLv1/CLv1Cart.java
@@ -13,47 +13,62 @@ public class CLv1Cart {
     // 2. 생성자
 
     // 3. 기능
+    // 장바구니에 메뉴를 추가하는 메서드
     public void cartsAdd() {
         System.out.println("\n");
-        System.out.println("\"" + cartsList.get(cartsList.size() - 1) + "\"");
+        System.out.println("\"" + cartsList.get(cartsList.size() - 1) + "\""); // cartsList 에 담겨있는 마지막 값을 보여준다.(선택한 메뉴가 cartsList 마지막에 저장되어 있다.)
         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
         System.out.println("1. 확인        2. 취소");
 
         Scanner scanner = new Scanner(System.in);
-        int cartNum = scanner.nextInt();
-        if (cartNum == 2) {
-            cartsList.remove(cartsList.size() - 1);
-        } else if (cartNum == 1) {
-            System.out.println(cartsList.get(cartsList.size() - 1).getName() + " 이 장바구니에 추가되었습니다.");
+        int cartNum = 0; // 장바구니에 추가 여부를 위한 변수 생성
+        while(cartNum != 1 && cartNum !=2) {
+            cartNum = scanner.nextInt();
+            if (cartNum == 2) { // 2를 입력하면 장바구니에 추가하지 않기 때문에 cartsList 에 있는 마지막 값을 삭제한다.
+                cartsList.remove(cartsList.size() - 1);
+            } else if (cartNum == 1) { // 1을 입력하면 장바구니에 추가된 메뉴 이름을 출력한다.
+                System.out.println(cartsList.get(cartsList.size() - 1).getName() + " 이 장바구니에 추가되었습니다.");
+            } else {
+                System.out.println("올바른 값을 입력해주세요");
+            }
         }
     }
 
+    // ORDER MENU 를 출력하는 메서드
     public void orderMenu() {
         System.out.println("[ ORDER MENU ]\n" +
                 "4. Orders       | 장바구니를 확인 후 주문합니다.\n" +
                 "5. Cancel       | 진행중인 주문을 취소합니다.");
     }
 
+    // 진행중인 주문을 취소하는 메서드
     public void cancel() {
-        cartsList.clear();
+        cartsList.clear(); // cartsList 에 담겨 있는 모든 값을 제거한다.
     }
 
+    // 장바구니를 확인 후 주문하는 메서드
     public void orders() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("아래와 같이 주문 하시겠습니까?");
+        int finalOrd = 0; // 최종으로 메뉴를 주문하기 위한 변수 생성
+        double sumResult = 0; // 주문할 가격의 합을 나타내기 위한 변수 생성
+        while(finalOrd != 1 && finalOrd !=2) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\n" + "아래와 같이 주문 하시겠습니까?");
+            System.out.println("[ Orders ]\n" + cartsList + "\n"); // 장바구니에 담겨 있는 메뉴를 보여준다.
 
-        int size = cartsList.size();
-        System.out.println("[ Orders ]\n" + cartsList + "\n");
+            int size = cartsList.size(); // cartsList 의 사이즈 변수 생성
+            double sum = 0; // total 금액 변수 생성
+            for (int i = 0; i < size; i++) { // cartsList 에 있는 가격의 합을 for 문을 활용해 구한다.
+                sum += cartsList.get(i).getPrice();
+            }
+            sumResult = Math.round(sum * 10) / 10.0; // 가격의 합을 소숫점 첫째자리까지 나타내기 위해 Math.round 를 사용한다.
+            System.out.println("[ Total ]" + "\n" + "W " + sumResult + "\n");
 
-        double sum = 0;
-        for (int i = 0; i < size; i++) {
-            sum += cartsList.get(i).getPrice();
+            System.out.println("1. 주문      2. 메뉴판");
+            finalOrd = scanner.nextInt(); // 최종으로 메뉴를 주문하기 위해 숫자를 입력 받는다.
+            if(finalOrd!=1 && finalOrd!=2){
+                System.out.println("올바른 값을 입력해주세요");
+            }
         }
-        double sumResult = Math.round(sum * 100) / 100.0;
-        System.out.println("[ Total ]" + "\n" + "W " + sumResult + "\n");
-
-        System.out.println("1. 주문      2. 메뉴판");
-        int finalOrd = scanner.nextInt();
 
         if (finalOrd == 1) {
             System.out.println("\n" + "주문이 완료되었습니다. 금액은 W " + sumResult + "입니다.");
@@ -61,7 +76,7 @@ public class CLv1Cart {
         }
     }
 
-
+    // cartsList 의 getter 메서드
     public List<CLv1MenuItem> getCarts() {
         return cartsList;
     }

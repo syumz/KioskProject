a1d985ef9de30f55e0363e6f77ae3fc255f05c74
seoyeon <dlatjdus0303@naver.com>
2025-04-29 19:46:12 +0900

    feat: add getters for name and per

--- src/ChallengeLv2/DiscountRatePerType.java
+++ src/ChallengeLv2/DiscountRatePerType.java
@@ -5,43 +5,43 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum DiscountRatePerType {
-    국가유공자("국가유공자", 10),
-    군인("군인", 5),
-    학생("학생", 3),
+    국가유공자("국가유공자", 0.1),
+    군인("군인", 0.05),
+    학생("학생", 0.03),
     일반("일반", 0);
 
     // 1. 속성
     private String name;
-    private int per;
-
-    // 2. 생성자
-    DiscountRatePerType(String name, int per){
-        this.name = name;
-        this.per = per;
-    }
-
-    public int getPer() {
-        return per;
-    }
-
-    public String getName() {
-        return name;
-    }
+    private double per;
 
     static List<String> discountName = Stream.of(DiscountRatePerType.values())
             .map(DiscountRatePerType::getName)
             .collect(Collectors.toList());
 
-    static List<Integer> discountPer = Stream.of(DiscountRatePerType.values())
+    static List<Double> discountPer = Stream.of(DiscountRatePerType.values())
             .map(DiscountRatePerType::getPer)
             .collect(Collectors.toList());
 
+    // 2. 생성자
+    DiscountRatePerType(String name, double per){
+        this.name = name;
+        this.per = per;
+    }
+
     // 3. 기능(메서드)
-    public static void discount(){
-        System.out.println("할인 정보를 입력해주세요");
+    public static void discountinfo(){
+        System.out.println("\n할인 정보를 입력해주세요");
         for (int i = 0; i < discountName.size(); i++) {
             System.out.print(i + 1 + ". ");
-            System.out.println(discountName.get(i) + ": "+ discountPer.get(i) + "%");
+            System.out.println(discountName.get(i) + ": "+ Math.round(discountPer.get(i)*100) + "%");
         }
+    }
+
+    public double getPer() {
+        return per;
+    }
+
+    public String getName() {
+        return name;
     }
 }
